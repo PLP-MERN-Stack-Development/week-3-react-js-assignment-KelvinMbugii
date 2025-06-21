c178cbc15761d4c0a210bbff6ffba78aae4d4abd
KelvinMbugii <wanjuguKelvin851@gmail.com>
2025-06-21 12:49:10 +0300

    added the correct base name in vite config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/https://github.com/PLP-MERN-Stack-Development/week-3-react-js-assignment-KelvinMbugii.git/",
+  base: "PLP-MERN-Stack-Development/week-3-react-js-assignment-KelvinMbugii",
 });
